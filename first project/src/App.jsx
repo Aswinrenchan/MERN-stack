b2a13762e06234c441d47ad180f3db5185096705
Aswinrenchan <aswinrenchan1@gmail.com>
2024-11-22 15:57:37 +0530

    initial commit

---
+++ first project/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+// import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <h1>Hello World</h1>
+    <h2>HY World</h2>
+    
+    </>
+    
+  )
+  }
+
+
+export default App
